3a4b1145312903b936276f47eab1f4cb47253731
Brian Mowrey <brian@drlabs.org>
2013-04-21 14:35:29 -0500

    initial commit

---
+++ lib/DBIx/Class/Storage/Statistics/Log4perl.pm
@@ -0,0 +1,39 @@
+package DBIx::Class::Storage::Statistics::Log4perl;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+DBIx::Class::Storage::Statistics::Log4perl - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use DBIx::Class::Storage::Statistics::Log4perl;
+
+=head1 DESCRIPTION
+
+DBIx::Class::Storage::Statistics::Log4perl is
+
+=head1 AUTHOR
+
+Brian Mowrey E<lt>brian@drlabs.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Brian Mowrey
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
